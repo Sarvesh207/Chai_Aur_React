3f0099f6dbec83ad4984a12e4994a2a04430990d
Sarvesh207 <sarveshgaynar@gmail.com>
2023-09-30 20:03:00 +0530

    currency converter app

--- 06CustomeHooks/src/hooks/useCurrencyInfo.js
+++ 06CustomeHooks/src/hooks/useCurrencyInfo.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
     const[data, setData] = useState({})
+    console.log(data)
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data)
+        
     },[currency])
-
+    
     return data;
 }
 
