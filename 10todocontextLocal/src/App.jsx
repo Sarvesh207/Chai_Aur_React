b27b3bc777434cd35c2c01c67dd2b3433d42ebb1
Sarvesh207 <sarveshgaynar@gmail.com>
2023-10-13 07:38:04 +0530

    started with rtk

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -4,8 +4,6 @@ import { TodoProvider } from "./contexts";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
 
-
-
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -54,18 +52,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}
-            <TodoForm/>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
-            {
-              todos.map((todo) => (
-                <div className="w-full" key={todo.id}>
-                  <TodoItem todo={todo}/>
-                </div>
-              ))
-            }
+            {todos.map((todo) => (
+              <div className="w-full" key={todo.id}>
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
