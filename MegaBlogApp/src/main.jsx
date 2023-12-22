17375d592ff014b50680623c5d5aacab5acb8c40
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-22 23:55:09 +0530

    styleing

--- MegaBlogApp/src/main.jsx
+++ MegaBlogApp/src/main.jsx
@@ -1,14 +1,68 @@
+import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import Login from "./components/Login.jsx";
+import { AuthLayout, Signup } from "./components/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {""}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post:slug",
+        element: (
+          <AuthLayout authentication>
+            {""}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider route={router} />
     </Provider>
   </React.StrictMode>
 );
